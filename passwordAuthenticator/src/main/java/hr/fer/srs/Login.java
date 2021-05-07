e048b85be539e66ce5d382b8a675dd06ba612789
Mirta Moslavac <mirta.moslavac@gmail.com>
2021-05-07 19:05:59 +0200

    create authenticator entry points

---
+++ passwordAuthenticator/src/main/java/hr/fer/srs/Login.java
@@ -0,0 +1,30 @@
+package hr.fer.srs;
+
+import hr.fer.srs.commands.*;
+import hr.fer.srs.elements.*;
+import hr.fer.srs.exceptions.*;
+
+import java.nio.file.*;
+
+public class Login {
+    public static void main(String[] args) {
+        try {
+            if (args.length != 2) throw new IllegalArgumentException("Invalid number of arguments.");
+
+            if (!Files.exists(Paths.get(FileUtils.PATH_STRING))) FileUtils.initializeFile();
+            FileUtils.checkCompromised();
+
+            ICommand command = switch (args[0]) {
+                case "login" -> new LoginUserCommand();
+                default -> throw new IllegalArgumentException("Illegal command " + args[0] + ".");
+            };
+            command.execute(args[1]);
+
+            FileUtils.storeChecksumOfFile();
+        } catch (PasswordAuthenticatorCompromisedException e) {
+            System.err.println("The password authenticator had been compromised.");
+        } catch (IllegalArgumentException | NullPointerException | PasswordAuthenticatorException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+}
